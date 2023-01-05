71e38521fbc7617decbcfaf915dd1b0df38bfe17
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-01-05 17:54:38 -0300

    🧑‍💻 chore: config path mapping

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+	mode: 'development',
+	resolve: {
+		alias: {
+			'@data': path.resolve(__dirname, './src/data'),
+			'@domain': path.resolve(__dirname, './src/domain'),
+			'@infra': path.resolve(__dirname, './src/infra'),
+			'@main': path.resolve(__dirname, './src/main'),
+			'@presentation': path.resolve(__dirname, './src/presentation'),
+		}
+	}
 })
