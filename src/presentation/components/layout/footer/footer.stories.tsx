5f2a09d0e5c8bc9c59b493a676b90b5500beaa21
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-01-06 16:17:36 -0300

    📝 docs: add Layout Components stories

---
+++ src/presentation/components/layout/footer/footer.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Footer } from '@presentation/components/layout';
+
+export default {
+	title: 'Components/Layout/Footer',
+	component: Footer
+} as Meta;
+
+export const Default: StoryObj = {};
---
+++ src/presentation/components/layout/header/header.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Header } from '@presentation/components/layout';
+
+export default {
+	title: 'Components/Layout/Header',
+	component: Header
+} as Meta;
+
+export const Default: StoryObj = {};
