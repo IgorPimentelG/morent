338a082cb3921b85ff7d4df753edbf3955ab726e
Igor Pimentel <igor.pimentel.msi@hotmail.com>
2023-01-14 19:50:14 -0300

    ✅ test: ensure loadRecomendationCars works

--- src/data/usecases/load-cars/remote-load-cars.spec.ts
+++ src/data/usecases/load-cars/remote-load-cars.spec.ts
@@ -33,7 +33,7 @@ describe('RemoteLoadCars', () => {
 			const url = faker.internet.url();
 			const { remoteLoadCars, httpClientSpy } = makeSut({ url });
 			await remoteLoadCars.loadPopularCars();
-			expect(httpClientSpy.url).toBe(url);
+			expect(httpClientSpy.url).toBe(`${url}?type=popular`);
 			expect(httpClientSpy.method).toBe('GET');
 		});
 
@@ -59,13 +59,55 @@ describe('RemoteLoadCars', () => {
 			expect(httpResponse).toEqual(cars);
 		});
 
-		test('Should thow ServerError when return status cpde 500', async () => {
+		test('Should thow ServerError when return status code 500', async () => {
 			const httpClientSpy = new HttpClientSpy();
 			httpClientSpy.response = {
 				statusCode: HttpStatus.serverError
 			};
 			const { remoteLoadCars } = makeSut({ httpClientSpy });
 			const promise = remoteLoadCars.loadPopularCars();
+			await expect(promise).rejects.toThrow(new ServerError());
+		});
+	});
+
+	describe('LoadRecomendationCars', () => {
+		test('Should call HttpClient with correct URL and method', async () => {
+			const url = faker.internet.url();
+			const { remoteLoadCars, httpClientSpy } = makeSut({ url });
+			await remoteLoadCars.loadRecomendationCars();
+			expect(httpClientSpy.url).toBe(`${url}?type=recomendation`);
+			expect(httpClientSpy.method).toBe('GET');
+		});
+
+		test('Should return an empty list when there are no cars', async () => {
+			const httpClientSpy = new HttpClientSpy();
+			httpClientSpy.response = {
+				statusCode: HttpStatus.noContent
+			};
+			const { remoteLoadCars } = makeSut({ httpClientSpy });
+			const httpResponse = await remoteLoadCars.loadRecomendationCars();
+			expect(httpResponse).toEqual([]);
+		});
+
+		test('Should return a list of cars successfully', async () => {
+			const cars = mockCars();
+			const httpClientSpy = new HttpClientSpy();
+			httpClientSpy.response = {
+				body: cars,
+				statusCode: HttpStatus.ok
+			};
+			const { remoteLoadCars } = makeSut({ httpClientSpy });
+			const httpResponse = await remoteLoadCars.loadRecomendationCars();
+			expect(httpResponse).toEqual(cars);
+		});
+
+		test('Should thow ServerError when return status code 500', async () => {
+			const httpClientSpy = new HttpClientSpy();
+			httpClientSpy.response = {
+				statusCode: HttpStatus.serverError
+			};
+			const { remoteLoadCars } = makeSut({ httpClientSpy });
+			const promise = remoteLoadCars.loadRecomendationCars();
 			await expect(promise).rejects.toThrow(new ServerError());
 		});
 	});
